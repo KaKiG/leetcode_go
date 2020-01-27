7256aeef90c55190c524cbb633214d4b5902db66
KaKiG <zeyuankn@gmail.com>
2020-01-26 21:28:58 -0500

    Update leetcode practice

---
+++ leetcode154.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{3, 1}
+	fmt.Println(findMin(nums))
+}
+
+func findMin(nums []int) int {
+	left, right, mid := 0, len(nums)-1, (len(nums)-1)/2
+
+	for left < right {
+		if nums[mid] > nums[right] {
+			left = mid + 1
+		} else if nums[mid] < nums[right] {
+			right = mid
+		} else {
+			right--
+		}
+		mid = (left + right) / 2
+	}
+	return nums[mid]
+}
